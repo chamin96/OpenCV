9bf25a09bcd2fcdd49d342d9fb3e577300b55fda
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2020-12-18 13:55:10 +0530

    reading a video

--- read.py
+++ read.py
@@ -1,7 +1,20 @@
 import cv2 as cv
 
-img = cv.imread('images/cat.jpg')
+# img = cv.imread('images/cat.jpg')
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
 
-cv.waitKey(0)
+# cv.waitKey(0)
+
+capture = cv.VideoCapture('videos/smiling.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
